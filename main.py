398842bf968da182508ca919ea4fca99f4b68423
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-09-27 10:50:13 +0300

    ИСПРАВЛЕННОЕ ЗАДАНИЕ  ошибка была в if result >= 1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def is_prime(func):
     def wrapper(*args,**kwargs):
         result = func(*args,**kwargs)
-        if result > 1:
+        if result >= 1:
             for i in range (2,int(result ** 0.5)+1): # проверка простое ли число 
                 if result % i == 0:
                     print('составное')
@@ -15,5 +15,5 @@ def is_prime(func):
 def sum_three(a,b,c):
     return a + b + c
 
-result = sum_three(2, 3, 6)
+result = sum_three(1, 0, 0)
 print(result)
